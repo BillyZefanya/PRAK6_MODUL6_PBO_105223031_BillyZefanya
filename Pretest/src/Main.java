2e5441de31d8aa03967887fe2a780a7140a9225d
BillyZefanya <billyzefanya2006@gmail.com>
2026-04-07 15:20:59 +0700

    first commit

---
+++ Pretest/src/Main.java
@@ -0,0 +1,8 @@
+import minimarket.MakananRingan;
+
+public class Main {
+    public static void main(String[] args) {
+        MakananRingan snack = new MakananRingan("Chiki-chiki", 5000, "Balado");
+        snack.tampilkanDetail();
+    }
+}
